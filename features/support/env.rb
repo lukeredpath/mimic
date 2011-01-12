5ac88c61d270deede882074e98cd33ca6e33a226
Luke Redpath <contact@lukeredpath.co.uk>
2011-01-12 14:49:27 +0000

    Support use of an HTTP proxy when running Cucumber features to assist debugging.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), *%w[.. .. lib]))
 
+Before do
+  if test_proxy = ENV["MIMIC_TEST_PROXY"]
+    puts "(Using proxy #{test_proxy})"
+    HttpClient.use_proxy(test_proxy)
+  end  
+end
