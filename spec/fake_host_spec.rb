9d01b9ea28b19290272100b4b0b443cdc2a778d6
Luke Redpath <contact@lukeredpath.co.uk>
2011-01-12 15:04:58 +0000

    Fixed broken spec

--- spec/fake_host_spec.rb
+++ spec/fake_host_spec.rb
@@ -7,34 +7,34 @@ describe "Mimic::FakeHost" do
   
   it "should handle stubbed requests" do
     @host.get("/some/path")
-    @host.call(request_for("/some/path")).should return_rack_response(200, {}, "")
+    @host.call(request_for("/some/path")).should match_rack_response(200, {}, "")
   end
   
   it "should handle stubbed requests that return a response" do
     @host.get("/some/path").returning("hello world")
-    @host.call(request_for("/some/path")).should return_rack_response(200, {}, "hello world")
+    @host.call(request_for("/some/path")).should match_rack_response(200, {}, "hello world")
   end
   
   it "should handle stubbed requests that return a specific HTTP code" do
     @host.get("/some/path").returning("redirecting", 301)
-    @host.call(request_for("/some/path")).should return_rack_response(301, {}, "redirecting")
+    @host.call(request_for("/some/path")).should match_rack_response(301, {}, "redirecting")
   end
   
   it "should handle stubbed requests that return specific headers" do
     @host.get("/some/path").returning("redirecting", 301, {"Location" => "http://somewhereelse.com"})
-    @host.call(request_for("/some/path")).should return_rack_response(301, {"Location" => "http://somewhereelse.com"}, "redirecting")
+    @host.call(request_for("/some/path")).should match_rack_response(301, {"Location" => "http://somewhereelse.com"}, "redirecting")
   end
   
   it "should not recognize requests if they have the incorrect HTTP method" do
     @host.get("/some/path")
-    @host.call(request_for("/some/path", :method => "POST")).should return_rack_response(404, {}, "")
+    @host.call(request_for("/some/path", :method => "POST")).should match_rack_response(404, {}, "")
   end
   
   it "should not handle multiple requests to a path with different HTTP methods" do
     @host.get("/some/path").returning("GET Request", 200)
     @host.post("/some/path").returning("POST Request", 201)
-    @host.call(request_for("/some/path", :method => "GET")).should return_rack_response(200, {}, "GET Request")
-    @host.call(request_for("/some/path", :method => "POST")).should return_rack_response(201, {}, "POST Request")
+    @host.call(request_for("/some/path", :method => "GET")).should match_rack_response(200, {}, "GET Request")
+    @host.call(request_for("/some/path", :method => "POST")).should match_rack_response(201, {}, "POST Request")
   end
   
   it "should handle requests with behaviour specified in a block using the Sinatra API" do
@@ -42,7 +42,7 @@ describe "Mimic::FakeHost" do
       content_type 'text/plain'
       'bobby'
     end
-    @host.call(request_for("/some/path", :method => "GET")).should return_rack_response(200, {'Content-Type' => 'text/plain'}, 'bobby')
+    @host.call(request_for("/some/path", :method => "GET")).should match_rack_response(200, {'Content-Type' => 'text/plain;charset=utf-8'}, 'bobby')
   end
   
   private
@@ -55,7 +55,7 @@ describe "Mimic::FakeHost" do
       "rack.input"     => StringIO.new }
   end
   
-  RSpec::Matchers.define :return_rack_response do |code, headers, body|
+  RSpec::Matchers.define :match_rack_response do |code, headers, body|
     match do |actual|
       (actual[0].should == code) &&
       (actual[1].should include(headers)) &&
