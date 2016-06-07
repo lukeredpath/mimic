8cd25d7c03ce448e642a8b96a9252868dcc9bc00
Luke Redpath <luke@lukeredpath.co.uk>
2016-06-07 12:24:47 +0100

    Convert specs to RSpec 3.4.4 syntax with Transpec
    This conversion is done by Transpec 3.2.2 with the following command:
        transpec -f
    
    * 15 conversions
        from: obj.should
          to: expect(obj).to
    
    * 2 conversions
        from: == expected
          to: eq(expected)
    
    * 1 conversion
        from: proc { }.should
          to: expect { }.to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/fake_host_spec.rb
+++ spec/fake_host_spec.rb
@@ -7,34 +7,34 @@ describe "Mimic::FakeHost" do
   
   it "should handle stubbed requests" do
     @host.get("/some/path")
-    @host.call(request_for("/some/path")).should match_rack_response(200, {}, "")
+    expect(@host.call(request_for("/some/path"))).to match_rack_response(200, {}, "")
   end
   
   it "should handle stubbed requests that return a response" do
     @host.get("/some/path").returning("hello world")
-    @host.call(request_for("/some/path")).should match_rack_response(200, {}, "hello world")
+    expect(@host.call(request_for("/some/path"))).to match_rack_response(200, {}, "hello world")
   end
   
   it "should handle stubbed requests that return a specific HTTP code" do
     @host.get("/some/path").returning("redirecting", 301)
-    @host.call(request_for("/some/path")).should match_rack_response(301, {}, "redirecting")
+    expect(@host.call(request_for("/some/path"))).to match_rack_response(301, {}, "redirecting")
   end
   
   it "should handle stubbed requests that return specific headers" do
     @host.get("/some/path").returning("redirecting", 301, {"Location" => "http://somewhereelse.com"})
-    @host.call(request_for("/some/path")).should match_rack_response(301, {"Location" => "http://somewhereelse.com"}, "redirecting")
+    expect(@host.call(request_for("/some/path"))).to match_rack_response(301, {"Location" => "http://somewhereelse.com"}, "redirecting")
   end
   
   it "should not recognize requests if they have the incorrect HTTP method" do
     @host.get("/some/path")
-    @host.call(request_for("/some/path", :method => "POST")).should match_rack_response(404, {}, "")
+    expect(@host.call(request_for("/some/path", :method => "POST"))).to match_rack_response(404, {}, "")
   end
   
   it "should not handle multiple requests to a path with different HTTP methods" do
     @host.get("/some/path").returning("GET Request", 200)
     @host.post("/some/path").returning("POST Request", 201)
-    @host.call(request_for("/some/path", :method => "GET")).should match_rack_response(200, {}, "GET Request")
-    @host.call(request_for("/some/path", :method => "POST")).should match_rack_response(201, {}, "POST Request")
+    expect(@host.call(request_for("/some/path", :method => "GET"))).to match_rack_response(200, {}, "GET Request")
+    expect(@host.call(request_for("/some/path", :method => "POST"))).to match_rack_response(201, {}, "POST Request")
   end
   
   it "should handle requests with behaviour specified in a block using the Sinatra API" do
@@ -42,47 +42,47 @@ describe "Mimic::FakeHost" do
       content_type 'text/plain'
       'bobby'
     end
-    @host.call(request_for("/some/path", :method => "GET")).should match_rack_response(200, {'Content-Type' => 'text/plain;charset=utf-8'}, 'bobby')
+    expect(@host.call(request_for("/some/path", :method => "GET"))).to match_rack_response(200, {'Content-Type' => 'text/plain;charset=utf-8'}, 'bobby')
   end
   
   it "should allow stubs to be cleared" do
     @host.get("/some/path")
-    @host.call(request_for("/some/path")).should match_rack_response(200, {}, "")
+    expect(@host.call(request_for("/some/path"))).to match_rack_response(200, {}, "")
     @host.clear
-    @host.call(request_for("/some/path")).should match_rack_response(404, {}, "")
+    expect(@host.call(request_for("/some/path"))).to match_rack_response(404, {}, "")
   end
   
   it "should allow stubs to be imported from a file" do
     @host.import(File.join(File.dirname(__FILE__), *%w[fixtures import_stubs.mimic]))
-    @host.call(request_for("/imported/path")).should match_rack_response(200, {}, "")
+    expect(@host.call(request_for("/imported/path"))).to match_rack_response(200, {}, "")
   end
   
   it "should not clear imported stubs" do
     @host.import(File.join(File.dirname(__FILE__), *%w[fixtures import_stubs.mimic]))
     @host.clear
-    @host.call(request_for("/imported/path")).should match_rack_response(200, {}, "")
+    expect(@host.call(request_for("/imported/path"))).to match_rack_response(200, {}, "")
   end
   
   it "should raise if import file does not exist" do
-    proc { 
+    expect { 
       @host.import(File.join(File.dirname(__FILE__), *%w[fixtures doesnt_exist.mimic]))
-    }.should raise_error(RuntimeError)
+    }.to raise_error(RuntimeError)
   end
   
   it "returns a StubbedRequest" do
-    @host.get("/some/path").should be_kind_of(Mimic::FakeHost::StubbedRequest)
+    expect(@host.get("/some/path")).to be_kind_of(Mimic::FakeHost::StubbedRequest)
   end
   
   describe "StubbedRequest" do
     it "has a unique hash based on it's parameters" do
       host = Mimic::FakeHost::StubbedRequest.new(stub, "GET", "/path")
-      host.to_hash.should == Digest::MD5.hexdigest("GET /path")
+      expect(host.to_hash).to eq(Digest::MD5.hexdigest("GET /path"))
     end
     
     it "has the same hash as an equivalent request" do
       host_one = Mimic::FakeHost::StubbedRequest.new(stub, "GET", "/path")
       host_two = Mimic::FakeHost::StubbedRequest.new(stub, "GET", "/path")
-      host_one.to_hash.should == host_two.to_hash
+      expect(host_one.to_hash).to eq(host_two.to_hash)
     end
   end
   
